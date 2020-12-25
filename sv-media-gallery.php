494301e391dd9e42dac92620495696c9439be008
sovetit <pavel@sovetit.ru>
2020-12-25 20:41:34 +1000

    Added general settings and localization

--- sv-media-gallery.php
+++ sv-media-gallery.php
@@ -1,11 +1,13 @@
 <?php
 /**
  * Plugin Name: SV Media Gallery
- * Description: Галерея для сайта на основе плагина Carbon Fields
+ * Description: Gallery for a site based on the Carbon Fields plugin
  * Plugin URI:  https://sovetit.ru/wordpress/plugins/carbon-fields/media-gallery/
  * Author URI:  https://sovetit.ru/about/
- * Author:      Кетов Павел
- * Version:     1.0.0
+ * Author:      Pavel Ketov
+ * Version:     1.2.0
+ * Text Domain: sv_media_gallery
+ * Domain Path: /languages
  */
 defined( 'ABSPATH' ) || exit;
 
@@ -19,12 +21,28 @@ if ( ! defined( 'SV_GALLERY_PLUGIN_DIR' ) ) {
 	// Абсолютный путь к каталогу плагина
 	define( 'SV_GALLERY_PLUGIN_DIR', str_replace( '\\', '/', dirname( __FILE__ ) ) );
 
+	// Имя каталога плагина
+	define( 'SV_GALLERY_PLUGIN_NAME', basename( SV_GALLERY_PLUGIN_DIR ) );
+
 	// URL каталога плагина
-	define( 'SV_GALLERY_PLUGIN_URL', plugins_url( basename( SV_GALLERY_PLUGIN_DIR ) ) );
+	define( 'SV_GALLERY_PLUGIN_URL', plugins_url( SV_GALLERY_PLUGIN_NAME ) );
 
 	// Версия плагина
 	define( 'SV_GALLERY_VERSION', '1.0.0' );
 }
+
+/**
+ * Локализация плагина
+ * @see sv_gallery_languages
+ */
+function sv_gallery_languages() {
+	load_plugin_textdomain(
+		'sv_media_gallery',
+		false,
+		SV_GALLERY_PLUGIN_NAME . '/languages/'
+	);
+}
+add_action( 'plugins_loaded', 'sv_gallery_languages' );
 
 /**
  * Создаем новый тип записи Галерея
@@ -32,8 +50,8 @@ if ( ! defined( 'SV_GALLERY_PLUGIN_DIR' ) ) {
  */
 function sv_gallery_init() {
 	$labels = [
-		'name'               => 'Галереи',
-		'singular_name'      => 'Галерея',
+		'name'               => __( 'Galleries', 'sv_media_gallery' ), // Галереи
+		'singular_name'      => __( 'Gallery', 'sv_media_gallery' ), // Галерея
 	];
 	$args = [
 		'labels'             => $labels,
@@ -60,10 +78,10 @@ add_action( 'init', 'sv_gallery_init' );
  * @see sv_media_gallery
  */
 function sv_media_gallery() {
-	Container::make( 'post_meta', 'page-gallery', 'Галерея' )
+	Container::make( 'post_meta', 'page-gallery', __( 'Gallery', 'sv_media_gallery' ) )
 	         ->where( 'post_type', '=', 'sv_gallery' ) // Выводим только на типе записи галерея
 	         ->add_fields([
-			Field::make('media_gallery', 'photo', 'Фото' )
+			Field::make('media_gallery', 'photo', __( 'Photo', 'sv_media_gallery' ) )
 			     ->set_type('image')
 		])->set_context('normal');
 }
@@ -116,16 +134,42 @@ function sv_gallery_scripts() {
 
 	if ( has_shortcode( get_the_content(), 'sv-gallery' ) ) {
 
+		// Общие настройки галереи, так же по умолчанию для страниц с использованием шорткод(ов)
 		$is_gallery = true;
 
-		// Настройки по умолчанию для страниц с шорткодом
-		$gallery_option_mode                    = 'lg-slide';    // Эффект перехода между изображениями
-		$gallery_option_thumbnail               = 1;             // Включить миниатюры для галереи
-		$gallery_option_animate_thumb           = 1;             // Включить анимацию миниатюр
-		$gallery_option_show_thumb_by_default   = 1;             // Показать эскизы
-		$gallery_option_share                   = 0;             // Отключить кнопки поделиться
-		$gallery_option_download                = 0;             // Скрыть кнопку загрузки изображения
-		$gallery_option_preload                 = 2;             // Предварительно зкгрузить 2 изображения
+		// Эффект
+		$mode = carbon_get_theme_option( 'mode' );
+		$gallery_option_mode = ( empty( $mode ) ) ? 'lg-slide' : $mode;
+
+		// Миниатюры
+		$gallery_option_thumbnail = (
+		empty( carbon_get_theme_option( 'thumbnail' ) )
+		) ? 0 : 1;
+
+		// Анимация
+		$gallery_option_animate_thumb = (
+		empty( carbon_get_theme_option( 'animate_thumb' ) )
+		) ? 0 : 1;
+
+		// Эскизы
+		$gallery_option_show_thumb_by_default = (
+		empty( carbon_get_theme_option( 'show_thumb_by_default' ) )
+		) ? 0 : 1;
+
+		// Кнопки поделиться
+		$gallery_option_share = (
+		empty( carbon_get_theme_option(  'share' ) )
+		) ? 0 : 1;
+
+		// Кнопка загрузки изображения
+		$gallery_option_download = (
+		empty( carbon_get_theme_option(  'download' ) )
+		) ? 0 : 1;
+
+		// Количество изображений
+		$preload = carbon_get_theme_option( 'preload' );
+		$gallery_option_preload = ( empty( $preload ) ) ? 2 : $preload;
+
 	} else {
 
 		$is_gallery = false;
@@ -238,7 +282,7 @@ add_action( 'wp_enqueue_scripts', 'sv_gallery_scripts' );
 function sv_gallery_short_code( $columns ) {
 	$num = 2; // после какой по счету колонки вставлять
 	$new_columns = array(
-		'short_code' => 'Шорткод',
+		'short_code' => __( 'Short code', 'sv_media_gallery' ),
 	);
 	return array_slice( $columns, 0, $num ) + $new_columns + array_slice( $columns, $num );
 }
@@ -295,80 +339,142 @@ function sv_gallery_get_short_code( $args ) {
 add_shortcode( 'sv-gallery', 'sv_gallery_get_short_code' );
 
 /**
+ * Эффекты
+ * @see sv_gallery_field_mode
+ * @return array
+ */
+function sv_gallery_field_mode() {
+	$mode = [
+		'lg-slide'                        => 'Slide',
+		'lg-fade'                         => 'Fade',
+		'lg-zoom-in'                      => 'Zoom-in',
+		'lg-zoom-in-big'                  => 'Zoom-in-big',
+		'lg-zoom-out'                     => 'Zoom-out',
+		'lg-zoom-out-big'                 => 'Zoom-out-big',
+		'lg-zoom-out-in'                  => 'Zoom-out-in',
+		'lg-zoom-in-out'                  => 'Zoom-in-out',
+		'lg-soft-zoom'                    => 'Soft-zoom',
+		'lg-scale-up'                     => 'Scale-up',
+		'lg-slide-circular'               => 'Slide-circular',
+		'lg-slide-circular-vertical'      => 'Slide-circular-vertical',
+		'lg-slide-vertical'               => 'Slide-vertical',
+		'lg-slide-vertical-growth'        => 'Slide-vertical-growth',
+		'lg-slide-skew-only'              => 'Slide-skew-only',
+		'lg-slide-skew-only-rev'          => 'Slide-skew-only-rev',
+		'lg-slide-skew-only-y'            => 'Slide-skew-only-y',
+		'lg-slide-skew-only-y-rev'        => 'Slide-skew-only-y-rev',
+		'lg-slide-skew'                   => 'Slide-skew',
+		'lg-slide-skew-rev'               => 'Slide-skew-rev',
+		'lg-slide-skew-cross'             => 'Slide-skew-cross',
+		'lg-slide-skew-cross-rev'         => 'Slide-skew-cross-rev',
+		'lg-slide-skew-ver'               => 'Slide-skew-ver',
+		'lg-slide-skew-ver-rev'           => 'Slide-skew-ver-rev',
+		'lg-slide-skew-ver-cross'         => 'Slide-skew-ver-cross',
+		'lg-slide-skew-ver-cross-rev'     => 'Slide-skew-ver-cross-rev',
+		'lg-lollipop'                     => 'Lollipop',
+		'lg-lollipop-rev'                 => 'Lollipop-rev',
+		'lg-rotate'                       => 'Rotate',
+		'lg-rotate-rev'                   => 'Rotate-rev',
+		'lg-tube'                         => 'Tube'
+	];
+	return $mode;
+}
+
+/**
  * Настройки галереи
  * @see sv_gallery_options
  */
 function sv_gallery_options() {
-	Container::make( 'post_meta', 'sv-gallery-options', 'Настройки галереи' )
-		->where( 'post_type', '=', 'sv_gallery' )
-	         ->add_fields([
-		         Field::make( 'select', 'mode', 'Эффект' )
-		              ->add_options( [
-			              'lg-slide'                        => 'Slide',
-			              'lg-fade'                         => 'Fade',
-			              'lg-zoom-in'                      => 'Zoom-in',
-			              'lg-zoom-in-big'                  => 'Zoom-in-big',
-			              'lg-zoom-out'                     => 'Zoom-out',
-			              'lg-zoom-out-big'                 => 'Zoom-out-big',
-			              'lg-zoom-out-in'                  => 'Zoom-out-in',
-			              'lg-zoom-in-out'                  => 'Zoom-in-out',
-			              'lg-soft-zoom'                    => 'Soft-zoom',
-			              'lg-scale-up'                     => 'Scale-up',
-			              'lg-slide-circular'               => 'Slide-circular',
-			              'lg-slide-circular-vertical'      => 'Slide-circular-vertical',
-			              'lg-slide-vertical'               => 'Slide-vertical',
-			              'lg-slide-vertical-growth'        => 'Slide-vertical-growth',
-			              'lg-slide-skew-only'              => 'Slide-skew-only',
-			              'lg-slide-skew-only-rev'          => 'Slide-skew-only-rev',
-			              'lg-slide-skew-only-y'            => 'Slide-skew-only-y',
-			              'lg-slide-skew-only-y-rev'        => 'Slide-skew-only-y-rev',
-			              'lg-slide-skew'                   => 'Slide-skew',
-			              'lg-slide-skew-rev'               => 'Slide-skew-rev',
-			              'lg-slide-skew-cross'             => 'Slide-skew-cross',
-			              'lg-slide-skew-cross-rev'         => 'Slide-skew-cross-rev',
-			              'lg-slide-skew-ver'               => 'Slide-skew-ver',
-			              'lg-slide-skew-ver-rev'           => 'Slide-skew-ver-rev',
-			              'lg-slide-skew-ver-cross'         => 'Slide-skew-ver-cross',
-			              'lg-slide-skew-ver-cross-rev'     => 'Slide-skew-ver-cross-rev',
-			              'lg-lollipop'                     => 'Lollipop',
-			              'lg-lollipop-rev'                 => 'Lollipop-rev',
-			              'lg-rotate'                       => 'Rotate',
-			              'lg-rotate-rev'                   => 'Rotate-rev',
-			              'lg-tube'                         => 'Tube'
-		              ])->set_help_text( 'Эффект перехода между изображениями' ),
+	Container::make( 'post_meta', 'sv-gallery-options', __( 'Gallery settings', 'sv_media_gallery' ) )
+         ->where( 'post_type', '=', 'sv_gallery' )
+         ->add_fields([
+	         Field::make( 'select', 'mode', __( 'Effect', 'sv_media_gallery' ) )
+	              ->add_options(
+	              	sv_gallery_field_mode()
+	              )->set_help_text( __( 'Image transition effect', 'sv_media_gallery' ) ),
 
-		         Field::make( 'checkbox', 'thumbnail', 'Миниатюры' )
-		              ->set_option_value( 'yes' )
-			         ->set_default_value( 'yes' ) // По умолчанию миниатюры для галереи включены
-		              ->set_help_text( 'Включить миниатюры для галереи' ),
+	         Field::make( 'checkbox', 'thumbnail', __( 'Miniatures', 'sv_media_gallery' ) )
+	              ->set_option_value( 'yes' )
+	              ->set_default_value( 'yes' ) // По умолчанию миниатюры для галереи включены
+	              ->set_help_text( __( 'Enable gallery thumbnails', 'sv_media_gallery' ) ),
 
-		         Field::make( 'checkbox', 'animate_thumb', 'Анимация' )
-		              ->set_option_value( 'yes' )
-			         ->set_default_value( 'yes' ) // По умолчанию анимация миниатюр включена
-		              ->set_help_text( 'Включить анимацию миниатюр' ),
+	         Field::make( 'checkbox', 'animate_thumb', __( 'Animation', 'sv_media_gallery' ) )
+	              ->set_option_value( 'yes' )
+	              ->set_default_value( 'yes' ) // По умолчанию анимация миниатюр включена
+	              ->set_help_text( __( 'Enable thumbnail animation', 'sv_media_gallery' ) ),
 
-		         Field::make( 'checkbox', 'show_thumb_by_default', 'Эскизы' )
-		              ->set_option_value( 'yes' )
-		              ->set_default_value( 'yes' ) // По умолчанию эскизы включены
-		              ->set_help_text( 'Показать эскизы' ),
+	         Field::make( 'checkbox', 'show_thumb_by_default', __( 'Sketches', 'sv_media_gallery' ) )
+	              ->set_option_value( 'yes' )
+	              ->set_default_value( 'yes' ) // По умолчанию эскизы включены
+	              ->set_help_text( __( 'Show thumbnails', 'sv_media_gallery' ) ),
 
-		         // По умолчанию эскизы выключены
-		         Field::make( 'checkbox', 'share', 'Кнопки поделиться' )
-		              ->set_option_value( 'yes' )
-		              ->set_help_text( 'Показать кнопки поделиться' ),
+	         // По умолчанию эскизы выключены
+	         Field::make( 'checkbox', 'share', __( 'Share buttons', 'sv_media_gallery' ) )
+	              ->set_option_value( 'yes' )
+	              ->set_help_text( __( 'Show share buttons', 'sv_media_gallery' ) ),
 
-		         // По умолчанию кнопка загрузки изображения скрыта
-		         Field::make( 'checkbox', 'download', 'Кнопка загрузки изображения' )
-		              ->set_option_value( 'yes' )
-		              ->set_help_text( 'Показать кнопку загрузки изображения' ),
+	         // По умолчанию кнопка загрузки изображения скрыта
+	         Field::make( 'checkbox', 'download', __( 'Image upload button', 'sv_media_gallery' ) )
+	              ->set_option_value( 'yes' )
+	              ->set_help_text( __( 'Show Image Upload Button', 'sv_media_gallery' ) ),
 
-		         Field::make( 'text', 'preload', 'Количество изображений' )
-		              ->set_attribute( 'type', 'number' )
-		              ->set_attribute( 'max', 10 )      // Максимальное кол-во
-		              ->set_attribute( 'min', 1 )       // Миниальное кол-во
-			         ->set_default_value( 2 )           // По умолчанию 2 изображения
-			         ->set_help_text( 'Количество изображений предварительной загрузки' ),
+	         Field::make( 'text', 'preload', __( 'Number of images', 'sv_media_gallery' ) )
+	              ->set_attribute( 'type', 'number' )
+	              ->set_attribute( 'max', 10 )      // Максимальное кол-во
+	              ->set_attribute( 'min', 1 )       // Миниальное кол-во
+	              ->set_default_value( 2 )          // По умолчанию 2 изображения
+	              ->set_help_text( __( 'Number of preload images', 'sv_media_gallery' ) ),
 
-	         ])->set_context('side');
+         ])->set_context('side');
 }
 add_action( 'carbon_fields_register_fields', 'sv_gallery_options' );
+
+/**
+ * Настройки поо умолчанию
+ * @see sv_gallery_options_default
+ */
+function sv_gallery_options_default() {
+	Container::make( 'theme_options', 'sv-gallery-options', __( 'General settings', 'sv_media_gallery' ) )
+		->set_page_parent( 'edit.php?post_type=sv_gallery' )
+		->set_page_file( 'general-settings' )
+		->add_fields([
+			Field::make( 'select', 'mode', __( 'Effect', 'sv_media_gallery' ) )
+			     ->add_options(
+				sv_gallery_field_mode()
+			)->set_help_text( __( 'Image transition effect', 'sv_media_gallery' ) ),
+
+			Field::make( 'checkbox', 'thumbnail', __( 'Miniatures', 'sv_media_gallery' ) )
+			     ->set_option_value( 'yes' )
+			     ->set_default_value( 'yes' ) // По умолчанию миниатюры для галереи включены
+			     ->set_help_text( __( 'Enable gallery thumbnails', 'sv_media_gallery' ) ),
+
+			Field::make( 'checkbox', 'animate_thumb', __( 'Animation', 'sv_media_gallery' ) )
+			     ->set_option_value( 'yes' )
+			     ->set_default_value( 'yes' ) // По умолчанию анимация миниатюр включена
+			     ->set_help_text( __( 'Enable thumbnail animation', 'sv_media_gallery' ) ),
+
+			Field::make( 'checkbox', 'show_thumb_by_default', __( 'Sketches', 'sv_media_gallery' ) )
+			     ->set_option_value( 'yes' )
+			     ->set_default_value( 'yes' ) // По умолчанию эскизы включены
+			     ->set_help_text( __( 'Show thumbnails', 'sv_media_gallery' ) ),
+
+			// По умолчанию эскизы выключены
+			Field::make( 'checkbox', 'share', __( 'Share buttons', 'sv_media_gallery' ) )
+			     ->set_option_value( 'yes' )
+			     ->set_help_text( __( 'Show share buttons', 'sv_media_gallery' ) ),
+
+			// По умолчанию кнопка загрузки изображения скрыта
+			Field::make( 'checkbox', 'download', __( 'Image upload button', 'sv_media_gallery' ) )
+			     ->set_option_value( 'yes' )
+			     ->set_help_text( __( 'Show Image Upload Button', 'sv_media_gallery' ) ),
+
+			Field::make( 'text', 'preload', __( 'Number of images', 'sv_media_gallery' ) )
+			     ->set_attribute( 'type', 'number' )
+			     ->set_attribute( 'max', 10 )      // Максимальное кол-во
+			     ->set_attribute( 'min', 1 )       // Миниальное кол-во
+			     ->set_default_value( 2 )          // По умолчанию 2 изображения
+			     ->set_help_text( __( 'Number of preload images', 'sv_media_gallery' ) ),
+
+		]);
+}
+add_action( 'carbon_fields_register_fields', 'sv_gallery_options_default' );
